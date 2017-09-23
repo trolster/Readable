7f6410d57c737ef1e2533b649358b82c2d31e3bf
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-09-23 15:17:27 +0200

    refactor: export all components from index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,12 @@
-export { default as Navigation } from "./Navigation";
 export { default as Post } from "./Post";
+export { default as PostItem } from "./PostItem";
+export { default as PostForm } from "./PostForm";
+export { default as PostStub } from "./PostStub";
 export { default as Posts } from "./Posts";
+export { default as Navigation } from "./Navigation";
+export { default as CommentForm } from "./CommentForm";
+export { default as CommentItem } from "./CommentItem";
+export { default as Sort } from "./Sort";
+export { default as Spinner } from "./Spinner";
+export { default as Votes } from "./Votes";
+export { default as DateFromTimestamp } from "./DateFromTimestamp";
