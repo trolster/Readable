75cfcd93d468a0c9e187527d7fe38f49e18c96b2
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-09-23 15:01:33 +0200

    refactor: simplify code in FourOhFour.js

--- src/containers/FourOhFour.js
+++ src/containers/FourOhFour.js
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class FourOhFour extends Component {
-  render() {
-    return (
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexFlow: "column",
-          height: "100vh",
-          color: "#444"
-        }}
-      >
-        <h1 style={{ fontSize: "5rem" }}>404!</h1>
-        <h2>The page you are looking for doesn't exits.</h2>
-      </div>
-    );
-  }
-}
+export default () => (
+  <div
+    style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexFlow: "column",
+      height: "100vh",
+      color: "#444"
+    }}
+  >
+    <h1 style={{ fontSize: "5rem" }}>404!</h1>
+    <h2>The page you are looking for doesn't exits.</h2>
+  </div>
+);
