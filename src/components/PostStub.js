c787430230a16363592db30f2e665864e61f145f
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-09-23 14:18:20 +0200

    refactor: add post stub component

---
+++ src/components/PostStub.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import moment from "moment";
+import { Comment, Header, Icon } from "semantic-ui-react";
+import { deletePost } from "../actions/posts";
+import Votes from "./Votes";
+
+class PostStub extends Component {
+  state = {
+    editing: false,
+    redirect: false
+  };
+
+  render() {
+    const post = this.props.posts.items[this.props.postId];
+    const comments = Object.values(this.props.comments.items);
+    const commentCount = comments.filter(
+      comment => comment.parentId === post.id
+    ).length;
+    return (
+      <div style={{ display: "flex", flexAlign: "row" }} key={post.id}>
+        <Votes postId={post.id} />
+        <Comment.Group>
+          <Comment style={{ marginLeft: "15px" }}>
+            <Comment.Avatar src="http://via.placeholder.com/35" />
+            <Comment.Content>
+              <Comment.Author>
+                {post.author}
+                <Comment.Metadata>
+                  posted {moment(post.timestamp).fromNow()}
+                </Comment.Metadata>
+              </Comment.Author>
+              <Comment.Metadata style={{ display: "block", margin: ".33em 0" }}>
+                <Icon
+                  name="comment outline"
+                  style={{ margin: "0 4px -4px 0" }}
+                />
+                {commentCount} Comments
+              </Comment.Metadata>
+              <Link to={`/${post.category}/${post.id}`}>
+                <Header as="h3" style={{ margin: ".33em 0" }}>
+                  {post.title}
+                </Header>
+              </Link>
+              <Comment.Text>{post.body.substring(0, 120)}...</Comment.Text>
+              <Comment.Actions>
+                <Comment.Action href={`/${post.category}/${post.id}`}>
+                  Read More...
+                </Comment.Action>
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+        </Comment.Group>
+      </div>
+    );
+  }
+}
+
+export default connect(state => state, {
+  deletePost
+})(PostStub);
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,69 +2,40 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import sortBy from "lodash.sortby";
 import startCase from "lodash.startcase";
-import moment from "moment";
 import { getPostsByCategory, setPostSort } from "../actions/posts";
 import { getCommentsByPostIdList } from "../actions/comments";
-import { Link } from "react-router-dom";
-import { Segment, Container, Header, Comment, Icon } from "semantic-ui-react";
-import Votes from "./Votes";
+import { Segment, Container, Header, Comment } from "semantic-ui-react";
 import Sort from "./Sort";
 import PostForm from "./PostForm";
+import PostStub from "./PostStub";
+import Spinner from "./Spinner";
 
 class Posts extends Component {
+  state = {
+    loaded: false
+  };
+
   componentDidMount() {
     this.props.getPostsByCategory(this.props.category).then(postIds => {
-      this.props.getCommentsByPostIdList(postIds);
+      this.props.getCommentsByPostIdList(postIds).then(() => {
+        this.setState({ loaded: true });
+      });
     });
   }
+
   render() {
     const { items, sortby } = this.props.posts;
     const posts = sortBy(Object.values(items), sortby).reverse();
-    const comments = Object.values(this.props.comments.items);
+    if (!this.state.loaded) {
+      return <Spinner />;
+    }
     return (
       <Segment basic>
         <Container text>
           <Header as="h3">{startCase(this.props.category)} Posts</Header>
           {posts.length > 1 && <Sort itemType="posts" />}
           <Comment.Group>
-            {posts.map(post => {
-              const commentCount = comments.filter(
-                comment => comment.parentId === post.id
-              ).length;
-              const { author, timestamp, body, title, id, category } = post;
-
-              return (
-                <div style={{ display: "flex", flexAlign: "row" }} key={id}>
-                  <Votes postId={post.id} />
-                  <Comment style={{ marginLeft: "15px" }}>
-                    <Comment.Avatar src="http://via.placeholder.com/35" />
-                    <Comment.Content>
-                      <Comment.Author>
-                        {author}
-                        <Comment.Metadata>
-                          posted {moment(timestamp).fromNow()}
-                        </Comment.Metadata>
-                      </Comment.Author>
-                      <Comment.Metadata
-                        style={{ display: "block", margin: ".33em 0" }}
-                      >
-                        <Icon
-                          name="comment outline"
-                          style={{ margin: "0 4px -4px 0" }}
-                        />
-                        {commentCount} Comments
-                      </Comment.Metadata>
-                      <Link to={`/${category}/${id}`}>
-                        <Header as="h3" style={{ margin: ".33em 0" }}>
-                          {title}
-                        </Header>
-                        <Comment.Text>{body.substring(0, 120)}...</Comment.Text>
-                      </Link>
-                    </Comment.Content>
-                  </Comment>
-                </div>
-              );
-            })}
+            {posts.map(post => <PostStub postId={post.id} key={post.id} />)}
           </Comment.Group>
           <PostForm category={this.props.category} />
         </Container>
